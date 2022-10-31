8ec3fb2ba65be027b9d68d747d8330eb74d4e195
ryan <ryan.machado@fatec.sp.gov.br>
2022-10-31 18:15:35 -0300

    feat: fix docker enviroment vars

--- scripts/be-node-dev.sh
+++ scripts/be-node-dev.sh
@@ -2,9 +2,16 @@
 
 echo "Install bash and execute 'wait-for-it.sh' script"
 apk add --update bash
-./scripts/wait-for-it.sh $PG_HOST:5432 --timeout=30 --strict -- echo "postgres up and running"
 
-npm i dotenv-cli
+./scripts/wait-for-it.sh $DB_HOST:5432 --timeout=30 --strict -- echo "Postgres is up"
+
+echo "Running migrations"
+
 npm run typeorm:migration
 npm run typeorm:up
+
+echo "Running server"
+
 npm run dev
+
+
