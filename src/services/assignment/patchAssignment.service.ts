2b41472fe9421e49430176b44684e6d333a9d346
ryan <ryan.lucas@sponte.com.br>
2022-11-15 19:17:56 -0300

    fix: typeError on assignment

--- src/services/assignment/patchAssignment.service.ts
+++ src/services/assignment/patchAssignment.service.ts
@@ -2,8 +2,6 @@ import { Class } from '../../entities/class';
 import { ApiError } from '../../middlewares/error';
 import { classRepository } from '../../repositories/class.repository';
 
-
 export class UpdateClassesService {
-    async execute(params: ParamsProps) {
-      
+    async execute(params: ParamsProps) {}
 }
