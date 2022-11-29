a832e1f2ef8e904431e43ebf9553260e45d368b5
ryan <ryan.lucas@sponte.com.br>
2022-11-29 18:22:29 -0300

    feat: change logic on class service

--- src/services/classes/createClasses.service.ts
+++ src/services/classes/createClasses.service.ts
@@ -1,16 +1,34 @@
 import { Class } from '../../entities/class';
+import { ApiError } from '../../middlewares/error';
+import { blockRepository } from '../../repositories/block.repository';
 import { classRepository } from '../../repositories/class.repository';
 
 export class CreateClassesService {
     async execute(classes: Class) {
+        const block = await blockRepository.findOne({
+            // @ts-ignore
+            where: { id: classes.block[0] },
+        });
+
+        if (!block) {
+            throw new ApiError('Block not found', 400);
+        }
+
         const maxOrder = await classRepository.query(
             'SELECT MAX("order") FROM class WHERE "blockId" = $1',
-            [classes.block],
+            [block.id],
         );
 
+        console.log(block.id);
+
         const newClass = classRepository.create({
-            ...classes,
+            name: classes.name,
+            description: classes.description,
             order: maxOrder[0].max + 1,
+            cover: classes.cover ?? '',
+            // add block to class
+            //@ts-ignore
+            block: block,
         });
 
         await classRepository.save(newClass);
